2dab70eee2b4b2b8acee8b84a2197cbd55c41dc6
John Prantalos <johnprantalos@gmail.com>
2015-05-30 19:36:36 +0300

    Update12

--- site/inc/gmap.php
+++ site/inc/gmap.php
@@ -22,18 +22,20 @@
     </style>
     <script src="https://maps.googleapis.com/maps/api/js?v=3.exp&signed_in=true"></script>
     <script>
-function initialize() {
-  var mapOptions = {
-    center: myLatlng
-  }
-  var map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
+    function initialize() {
+      var myLatlng = new google.maps.LatLng(37.98439254177114, 23.72922420501709);
+      var mapOptions = {
+        zoom: 12,
+        center: myLatlng
+      }
+      var map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
 
-  var marker = new google.maps.Marker({
-      position: myLatlng,
-      map: map,
-      title: 'Hello World!'
-  });
-}
+      var marker = new google.maps.Marker({
+          position: myLatlng,
+          map: map,
+          title: 'Hello World!'
+      });
+    }
 
 google.maps.event.addDomListener(window, 'load', initialize);
 
