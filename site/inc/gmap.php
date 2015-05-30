70d04cbb0f01863ab245012813380f7de6621e5a
John Prantalos <johnprantalos@gmail.com>
2015-05-30 18:41:50 +0300

    Update7

---
+++ site/inc/gmap.php
@@ -0,0 +1,7 @@
+<iframe
+  src="https://www.google.com/maps/embed?pb=!1m14!1m12!1m3!1d12579.075981496286!2d23.729811126253562!3d37.98252000048911!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!5e0!3m2!1sen!2s!4v1432987195384"
+  width="100%"
+  height="100%"
+  frameborder="0"
+  style="border:0">
+</iframe>
--- site/index.php
+++ site/index.php
@@ -60,13 +60,7 @@
           <div class="store-map">
             <h1>Participating Stores</h1>
             <h2>See all the stores that support the project.</h2>
-            <iframe
-              src="https://www.google.com/maps/embed?pb=!1m14!1m12!1m3!1d12579.075981496286!2d23.729811126253562!3d37.98252000048911!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!5e0!3m2!1sen!2s!4v1432987195384"
-              width="100%"
-              height="100%"
-              frameborder="0"
-              style="border:0">
-            </iframe>
+            <?php include 'inc/gmap.php'; ?>
           </div><!-- /Store Map -->
         </div><!-- /Main Form -->
       </div><!-- /Content -->
