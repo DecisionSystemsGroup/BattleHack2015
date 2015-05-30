453b5ab89437099370cca6b90e24e4faa470f2e5
polykarposT <vepolis94@hotmail.com>
2015-05-30 19:26:44 +0300

    Update11

--- site/inc/gmap.php
+++ site/inc/gmap.php
@@ -1,7 +1,45 @@
-<iframe
+<!--iframe
   src="https://www.google.com/maps/embed?pb=!1m14!1m12!1m3!1d12579.075981496286!2d23.729811126253562!3d37.98252000048911!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!5e0!3m2!1sen!2s!4v1432987195384"
   width="100%"
   height="100%"
   frameborder="0"
   style="border:0">
-</iframe>
+</iframe>-->
+
+
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta name="viewport" content="initial-scale=1.0, user-scalable=no">
+    <meta charset="utf-8">
+    <title>Simple markers</title>
+    <style>
+      html, body, #map-canvas {
+        height: 100%;
+        margin: 0px;
+        padding: 0px
+      }
+    </style>
+    <script src="https://maps.googleapis.com/maps/api/js?v=3.exp&signed_in=true"></script>
+    <script>
+function initialize() {
+  var mapOptions = {
+    center: myLatlng
+  }
+  var map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
+
+  var marker = new google.maps.Marker({
+      position: myLatlng,
+      map: map,
+      title: 'Hello World!'
+  });
+}
+
+google.maps.event.addDomListener(window, 'load', initialize);
+
+    </script>
+  </head>
+  <body>
+    <div id="map-canvas"></div>
+  </body>
+</html>
