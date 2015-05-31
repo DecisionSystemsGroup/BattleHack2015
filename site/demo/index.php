7119ed2282b1ac7588ac67c7409c9a644891d77a
John Prantalos <johnprantalos@gmail.com>
2015-05-31 11:33:54 +0300

    Update39

---
+++ site/demo/index.php
@@ -0,0 +1,41 @@
+<?php
+  session_start();
+  require_once '../inc/configuration.php';
+?>
+<!DOCTYPE html>
+<html>
+  <head>
+  <meta charset="utf-8">
+  <meta http-equiv="content-type" content="text/html; charset=UTF-8">
+  <title>Food4Charity</title>
+  <meta name = "viewport" content= "width=device-width, initial-scale=1.0">
+  <meta name = "author" content = "">
+  <meta name = "description" content = "">
+
+  <link href="../css/main.css" rel="stylesheet">
+  <link href="../css/bootstrap.css" rel="stylesheet">
+
+  <link href='http://fonts.googleapis.com/css?family=Indie+Flower' rel='stylesheet' type='text/css'>
+  </head>
+  <body>
+    <div class="container">
+      <div class="row">
+        <div class="col-md-12">
+          <div class="text-center">
+            <p style="font-size:30px; font-weight:bold;">31/05/2015</p>
+            <h1 style="font-size:80px;">Available Free Meals for today</h1>
+          </div>
+          <div class="row">
+            <div class="col-md-2 col-md-offset-2">
+              <img src="../img/MITSOS.png" class="img-rounded img-responsive" />
+            </div>
+            <div class="col-md-6">
+              <h1 style="font-size:80px;">Papoutsakia</h1>
+              <p style="font-size:40px;">Quantity: 5 meals</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </body>
+</html>
