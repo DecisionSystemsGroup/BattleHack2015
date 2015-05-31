0c80df36fc413b74bb7e399811a9c84d9bb0c325
John Prantalos <johnprantalos@gmail.com>
2015-05-31 04:21:18 +0300

    31

--- site/index.php
+++ site/index.php
@@ -46,13 +46,6 @@
         <h1>Food<span>4</span>Charity</h1>
         <h2><i>Pay the meal for the people that need it most.</i></h2>
       </div>
-      <div class="row">
-        <div class="col-md-10 col-md-offset-1 text-center">
-          <h1>Say no to hunger!</h1>
-          <h2>Provide a meal to someone that cannot pay for it.</h2>
-          <h3> Just choose your desired meal and quantity, pay with your paypal or credit card and make someone happy.</h3>
-        </div>
-      </div>
       <div class="header-stores">
       <div class="row">
         <h4 class="text-center">Choose from:</h4>
